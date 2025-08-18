832cfb451ea970d9293180e3dc49c840b9ce0ffd
Steven Rieder <s.rieder@uva.nl>
2025-08-18 14:09:52 +0200

    rename shell scripts

--- gen_cluster_of_stars_planets_and_disks.sh
+++ gen_cluster_of_stars_planets_and_disks.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 python make_plummer_sphere.py --nstars 16 --radius 0.1 -Q 0.3 -F plummer_N16R01pcQ03.amuse 
 python name_stars.py -f plummer_N16R01pcQ03.amuse --nstars 6 --name system -F plummer_N16R01pcQ03N.amuse
 python add_planets_oligarch.py -f plummer_N16R01pcQ03N.amuse --fplanets 1 --name system -F plummer_N16R01pcQ03NOligarch.amuse --rmin_disk 3 --rmax_disk 300
