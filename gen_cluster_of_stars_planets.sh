32302038a10c560c57bebc177d17dacfc391b0cc
Steven Rieder <s.rieder@uva.nl>
2025-08-19 13:33:00 +0200

    update scripts to use CLI executables

--- gen_cluster_of_stars_planets.sh
+++ gen_cluster_of_stars_planets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-python make_plummer_sphere.py --nstars 16 --radius 0.1 -Q 0.5 -F plummer_N16R01pcQ05.amuse 
-python name_stars.py -f plummer_N16R01pcQ05.amuse --nstars 8 --name system -F plummer_N16R01pcQ05N8.amuse
-python add_planets_oligarch.py -f plummer_N16R01pcQ05N8.amuse --fplanets 1 --name system -F plummer_N16R01pcQ05N8Oligarch.amuse --rmin_disk 30 --rmax_disk 3000
-python rotate.py -f plummer_N16R01pcQ05N8Oligarch.amuse -F plummer_N16R01pcQ05N8OligarchR.amuse --type star
+amuse_ic_make_plummer_sphere --nstars 16 --radius 0.1 -Q 0.5 -F plummer_N16R01pcQ05.amuse 
+amuse_ic_name_stars -f plummer_N16R01pcQ05.amuse --nstars 8 --name system -F plummer_N16R01pcQ05N8.amuse
+amuse_ic_add_planets_oligarch -f plummer_N16R01pcQ05N8.amuse --fplanets 1 --name system -F plummer_N16R01pcQ05N8Oligarch.amuse --rmin_disk 30 --rmax_disk 3000
+amuse_ic_rotate -f plummer_N16R01pcQ05N8Oligarch.amuse -F plummer_N16R01pcQ05N8OligarchR.amuse --type star
 
